fe9e4cefff30cc16a1e00c038333ac2279ea9a90
ryanvillanueva <ryanvillanueva@172.31.98.196>
2019-10-07 17:05:45 -0700

    Initial commit of Project

---
+++ realGit/src/realGit/main.java
@@ -0,0 +1,22 @@
+package realGit;
+
+public class main 
+{
+	
+	public static void main(String [] args)
+	{
+		
+		display();
+		
+	}//public static vou dmain END
+	
+	
+	public static void display()
+	{
+		
+		System.out.println("This is Setsuna F. Seie.");
+		System.out.println("Gundam: Exia, Signing on!");
+		
+	}//display END
+
+}//main END
